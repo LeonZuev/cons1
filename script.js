f920b241cc7336f58376b059d3834da6629cba7f
Leon Zuev <loonatune@gmail.com>
2023-05-10 18:16:58 +0300

    Finish lesson

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Деструктуризация
 // не сохраняем в массив, не используем лишний раз память
 // не создаём лишние константы
-console.log('===== Destructurisation =====');
+console.log('===== Destructure =====');
 const infoFullName = ['John', 'Doe'];
 const [firstName, lastName] = infoFullName; // деструктуризация: firstName('John') lasName('Doe')
 console.log(firstName, lastName)
@@ -30,3 +30,34 @@ arr1 = ['a', 'b', 'c'];
 arr2 = [...arr1, 'd', 'e', 'f'];
 console.log(arr2);
 
+console.log("===== Array's methods =====");
+
+arr1 = [1, 2, 3];
+let res = arr1.push(5); // добавляет в конец массива, возвращает новую длинну массива
+console.log(arr1); // 1, 2, 3, 5
+console.log(res); //4
+
+res = arr1.pop(); // удаляет элемент в конце массива, возвращает удалённый элемент
+console.log(arr1); // 1, 2, 3
+console.log(res); // 5
+
+res = arr1.shift(); // удаляет элемент в начале массива, возвращает новую длинну массива
+console.log(arr1); // 2, 3
+console.log(res); // 1
+
+res = arr1.unshift(0); // добавляет элемент в начало массива, возвращает длинну массива
+console.log(arr1); // 0, 2, 3
+console.log(res); // 3
+
+res = arr1.splice(1, 2, 8, 9, 10); // Удаляет элементы из исходного массива, возвращает удалённый элемент
+console.log(arr1); // 0, 8, 9, 10
+console.log(res); // 2, 3
+
+res = arr1.splice(1, 3); // Не изменяет исходный массив, возвращает удалённый элемент
+console.log(arr1); // 0, 8, 9, 10
+console.log(res); // 8, 9
+
+const combArray = arr1.concat(arr2);
+console.log(arr1); // 0, 8, 9
+console.log(arr2);
+console.log(combArray);
